b0a16a101b04e9e67e3ee8d46a7dd2f4765a65a5
Andreea Nastase <andreanastase@gmail.com>
2020-04-02 17:23:23 +0200

    Add keys to grid, create basic view to work with

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
 
-const App = () => <h1>Hello worldddd</h1>;
+const App = () => {
+  return (
+    <div id="drum-machine">
+      <h1>Hello Drum Machine</h1>
+      <div id="display">
+        <div class="drum-pad">Q</div>
+        <div class="drum-pad">W</div>
+        <div class="drum-pad">E</div>
+        <div class="drum-pad">A</div>
+        <div class="drum-pad">S</div>
+        <div class="drum-pad">D</div>
+        <div class="drum-pad">Z</div>
+        <div class="drum-pad">X</div>
+        <div class="drum-pad">C</div>
+      </div>
+
+      <div id="output">Key pressed</div>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
